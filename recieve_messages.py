3f3e18994aa0f430f37a1ce5aa6cc137cd20a0a7
cdsupina <cdsupina@gmail.com>
2016-12-05 18:55:02 -0500

    added passcode functionality and arduino communication

--- recieve_messages.py
+++ recieve_messages.py
@@ -1,21 +1,51 @@
 from flask import Flask,request
 from twilio import twiml
+import time
+import RPi.GPIO as GPIO
+import random
+
+relay = 12
+code = ""
 
 app = Flask(__name__)
 
 @app.route('/sms',methods=['POST'])
 
 def sms():
-    number = request.form['From']
-    message_body = request.form['Body']
+	number = request.form['From']
+	message_body = request.form['Body']
 
-    resp = twiml.Response()
-    resp.message('Hello {}, you said: {}'.format(number,message_body))
+	resp = twiml.Response()
 
-    if message_body == "open":
-        print("opening lock")
+	if message_body == str(code):
+		print("changing lock")
+		GPIO.output(relay, GPIO.HIGH)
+		time.sleep(0.5)
+		GPIO.output(relay, GPIO.LOW)
+		resp.message("Activate...")
+	else:
+		print("unrecognized command")
+		resp.message("Unrecognized command sent.")
 
-    return str(resp)
+
+	return str(resp)
+
+def generateCode(digits = 4):
+	result = ""
+	i = 0	
+	while i < digits:
+		result+=str(random.randrange(10))
+		i+=1
+	return result
+
+
+
+
 
 if __name__ == '__main__':
-    app.run()
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(relay, GPIO.OUT)
+	GPIO.output(relay, GPIO.LOW)
+	code = generateCode()
+	print(code)
+	app.run()
